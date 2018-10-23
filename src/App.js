b313bfe883f73177affd4edf1e11877ae5b05743
Richard Pena <penarichard53@gmail.com>
2018-10-23 10:26:34 -0700

    renamed navigation links

--- src/App.js
+++ src/App.js
@@ -32,8 +32,8 @@ class App extends Component {
         <nav className="navbar navbar-light">
           <ul className="nav navbar-nav">
             <li><Link to="/">Home</Link></li>
-            <li><Link to="/category">Category</Link></li>
-            <li><Link to="/products">Products</Link></li>
+            <li><Link to="/routines"></Routines></li>
+            <li><Link to="/videos">Videos</Link></li>
         </ul>
        </nav>
 
